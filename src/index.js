30cf8d6d28e57896124e5d9b7cdd9e039679e0fb
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-04-21 12:18:53 -0700

    added product context provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import ProductsContextProvider from "./context/products-context";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-<App />
-</BrowserRouter>);
+    <ProductsContextProvider>
+      <App />
+    </ProductsContextProvider>
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/shop/index.js
@@ -0,0 +1,18 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: "Green Soap",
+    description: "Lorem Ipsum",
+    imageUrl: "https://i.ibb.co/s2VSYwT/green-soap.png",
+    price: 7,
+  },
+  {
+    id: 2,
+    title: "Hero Soap",
+    description: "Lorem Ipsum",
+    imageUrl: "https://i.ibb.co/VYwZxrz/hero-soap.jpg",
+    price: 7,
+  },
+];
+
+export default SHOP_DATA;
